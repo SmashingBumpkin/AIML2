914d1424342239f4543989c09fca1a3accbf8707
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2024-03-01 10:54:05 +0100

    Virtual env

---
+++ jupyterServer.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\Charl\Dropbox\Sapienza\Year 2\AI and ML U2"
+start cmd /k "C:\Users\Charl\Programming\AIML2\python38\Scripts\activate.bat && jupyter notebook --no-browser --ip=0.0.0.0 --port=9999"
